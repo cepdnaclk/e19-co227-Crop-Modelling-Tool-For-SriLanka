1274b9f25b224a9ef1dc7fa99ed4e504b1670893
Shantosh M <e19371@eng.pdn.ac.lk>
2023-10-10 09:34:39 +0530

    graphs & selections added
    Cumulative and Probability excedence graph added. Front End selections added

--- Back End/connectboxs.php
+++ Back End/connectboxs.php
@@ -1,29 +1,138 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Graph page</title>
+    <title>Graph Page</title>
     <!-- Include Plotly.js library -->
     <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-</head>
-<body>
-    <h1>My Plot</h1>
+    <style>
+        body {
+            font-family: poppins;
+            padding: 0;
+            margin: 0;
+            background: url('images/banner2.jpg') no-repeat center center fixed; /* Replace 'agriculture-background.jpg' with your background image */
+            background-size: cover; /* Ensures the background covers the entire page */
+            color: white;
 
-    <!-- This is for to choose what graph that you want -->
-    <label for="select-data">Select Data:</label>
-    <select id="select-data">
-        <option value="Scatter">Scatter</option>
-        <option value="Box">Box</option>
-        <option value="Cumulative Distribution">Cumulative Distribution</option>
-        <option value="robability Exceedance">Probability Exceedance</option>
-        <!-- Add more options as needed -->
-    </select>
+            /* Add background color with opacity */
+            background-color: rgba(5, 37, 1, 0.1); /* Adjust the last value (0.5) for opacity (0 to 1) */
+        }
+        .header {
+            font-family: poppins;
+            padding: 20px 0px; /* Increased top and bottom padding */
+            background: #052501;
+            background-size: cover; /* Ensures the background covers the entire header */
+            color: white;
+            text-align: center; /* Center align text */
+        }
+
+        .header h1 {
+            font-size: 44px; /* Increase font size */
+            margin-bottom: 10px; /* Add space below the heading */            
+            color: #2bd598;
+        }
+
+        .subtitle {
+            font-size: 20px; /* Font size for the subtitle */
+            color: green;
+        }
+
+        /* Container for the dropdown and back button */
+        .dropdown-container {
+            display: flex;
+            justify-content: flex-start; /* Align items to the left */
+            align-items: center;
+            padding: 10px;
+            margin-left: 10px; /* Add left margin to move the container to the left */
+            margin-right: 10px; /* Add right margin to center-align the container */
+        }
+
+        /* Dropdown */
+        #select-data {
+            background-color: #197f5a; /* Dark green */
+            color: #fff; /* White text */
+            border: none;
+            border-radius: 5px;
+            padding: 10px;
+            cursor: pointer;
+            margin-right: 10px;
+        }
+
+        /* Dropdown options */
+        #select-data option {
+            background-color: #197f5a; /* Dark green */
+            color: #fff; /* White text */
+        }
+
+        /* Hover effect for dropdown options */
+        #select-data option:hover {
+            background-color: #23644e; /* Dark teal */
+        }
+
+        /* Back button */
+        .back-button {
+            background-color: #197f5a; /* Dark green */
+            color: #fff; /* White text */
+            border: none;
+            border-radius: 5px;
+            padding: 10px 20px;
+            cursor: pointer;
+            margin-left: auto; /* Push the button to the right */
+        }
+
+        /* Hover effect for back button */
+        .back-button:hover {
+            background-color: #23644e; /* Dark teal */
+        }
+
+        /* Footer */
+        footer {
+            background-color: #2bd598; /* Teal */
+            color: #fff; /* White text */
+            text-align: center;
+            padding: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="header">
+        <div class="container">
+            <h1>CROPLANKA</h1>
+            <div class="subtitle">D A T A  &nbsp &nbsp V I S U A L I Z E R</div>
+        </div>
+    </div>
+
     <br>
+    <!-- Container for dropdown and back button -->
+    <div class="dropdown-container">
+        <!-- This is for choosing the type of graph -->
+        <label for="select-data">SELECT GRAPH TYPE :&nbsp </label>
+        <select id="select-data">
+            <option value="Scatter" selected>SCATTER</option>
+            <option value="Box">BOX</option>
+            <option value="Cumulative Distribution">CUMULATIVE DISTRIBUTION</option>
+            <option value="Probability Exceedance">PROBABILITY EXCEEDANCE</option>
+            <!-- Add more options as needed -->
+        </select>
+        
+        <!-- Back button -->
+        <button class="back-button" onclick="goBack()">Back</button>
+    </div>
+
+    <script>
+        // JavaScript code here
+        function goBack() {
+            window.history.back();
+        }
+    </script>
+
+    <br> 
 
     <?php
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $database = "project";
+    $database = "227project";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $database);
@@ -36,11 +145,11 @@
         if (isset($_GET['resultTexts'])) {
             $resultTextsJSON = $_GET['resultTexts'];
             $selections = json_decode($resultTextsJSON, true); // Decode JSON into an array
-            // Now you can use the $resultTexts array in your PHP code
+            // Now you can use the $selections array in your PHP code
         }
-        foreach($selections as$selection){
+        foreach ($selections as $selection) {
             echo "&nbsp &nbsp &nbsp &#x2022 ";
-            echo ucfirst($selection) ;
+            echo ucfirst($selection);
             echo "<br>";
         }
 
@@ -52,30 +161,38 @@
         if ($result) {
             $data = array();
             $key = array();
-            
-        // Collect data for plotting
+            $cumulative = array();
+            $probability = array();
+
+            // Collect data for plotting
             while ($row = $result->fetch_assoc()) {
-                foreach($selections as$selection){
-                    if ($row['Key']==$selection){
-                
+                foreach ($selections as $selection) {
+                    if ($row['Key'] == $selection) {
+
                         $key[] = $row['Key'];
-                        $data[] = $row['Yield Date'];
+                        $data[] = $row['Rice_yield'];
+                        $probability[] = $row['Probability'];
+                        $cumulative[] = $row['Cumulative'];
+
                     }
                 }
             }
-        }else {
+        } else {
             echo "Error: " . $conn->error;
         }
     }
 
     $conn->close();
     ?>
+    
     <!-- Add a div element to hold the scatter plot -->
     <div id="chart"></div>
     <script>
         // Get the x and y arrays from PHP
         var xData = <?php echo json_encode($key); ?>;
         var yData = <?php echo json_encode($data); ?>;
+        var y2Data = <?php echo json_encode($probability); ?>;
+        var y3Data = <?php echo json_encode($cumulative); ?>;
 
         // Event listener for the select element
         document.getElementById('select-data').addEventListener('change', function () {
@@ -87,24 +204,42 @@
             } else if (selectedValue === 'Box') {
                 createBoxPlot(xData, yData);
             }
+            else if (selectedValue === 'Probability Exceedance') {
+                createProbabilityPlot(yData, y2Data,xData);
+            }else if (selectedValue === 'Cumulative Distribution') {
+                createCumulativePlot(yData, y3Data,xData);
+            }
             // Add more conditions for other chart types as needed
+        });
+        // Set default selected option on page load
+        document.addEventListener('DOMContentLoaded', function () {
+            var defaultSelectedValue = 'Scatter';
+            document.getElementById('select-data').value = defaultSelectedValue;
+
+            // Initialize the chart based on the default selection (scatter plot)
+            createScatterPlot(xData, yData);
         });
 
         // Initialize the chart based on the default selection (scatter plot)
-        createScatterPlot(xData, yData);
+        //createScatterPlot(xData, yData);
 
-        // Function to create a scatter plot using Plotly
+
+        // Function to create a scatter plot using Plotly with an agriculture theme
         function createScatterPlot(xData, yData) {
             var trace = {
                 x: xData,
                 y: yData,
                 mode: 'markers',
                 type: 'scatter',
-                name: 'Scatter Plot',
+                name: 'Scatter plot',
+                marker: {
+                    color: '#157954', // Green color for data points
+                    size: 8, // Adjust the size of markers
+                },
             };
 
             var layout = {
-                title: 'Scatter Plot',
+                title: 'SCATTER PLOT',
                 xaxis: { title: 'X-Axis' },
                 yaxis: { title: 'Y-Axis' },
                 paper_bgcolor: 'rgb(233, 233, 233)',
@@ -115,7 +250,7 @@
             Plotly.newPlot('chart', [trace], layout);
         }
 
-        // Function to create a box plot using Plotly
+        // Function to create a box plot using Plotly with an agriculture theme
         function createBoxPlot(xData, yData) {
             var trace = {
                 x: xData,
@@ -123,12 +258,12 @@
                 type: 'box',
                 name: 'Box Plot',
                 marker: {
-                    color: 'RGB(86, 168, 50)' // Change color
+                    color: '#157954', // Green color for the box plot
                 }
             };
 
             var layout = {
-                title: 'Box Plot',
+                title: 'BOX PLOT',
                 xaxis: { title: 'X-Axis' },
                 yaxis: { title: 'Y-Axis' },
                 paper_bgcolor: 'rgb(233, 233, 233)',
@@ -138,11 +273,120 @@
             // Create the box plot inside the 'chart' div
             Plotly.newPlot('chart', [trace], layout);
         }
-    </script>
 
+        function createProbabilityPlot(xData, yData,colordata) {
+    var traces = [];
+    var currentTrace = {
+        x: [],
+        y: [],
+        type: 'scatter',
+        mode: 'lines+markers',
+        name: colordata[0],
+        marker: {
+            color: getRandomColor(), // Initial color for the cumulative plot
+        },
+    };
+
+    for (var i = 0; i < xData.length; i++) {
+        currentTrace.x.push(xData[i]);
+        currentTrace.y.push(yData[i]);
+
+        // Check if the next y-value is smaller, indicating a new line
+        if (!(colordata[i + 1] == colordata[i]) ) {
+            traces.push(currentTrace);
+
+            currentTrace = {
+                x: [],
+                y: [],
+                type: 'scatter',
+                mode: 'lines+markers',
+                name: colordata[i+1],
+                marker: {
+                    color: getRandomColor(), // Assign a new color for the new line segment
+                },
+            };
+        }
+    }
+
+    // Push the last trace
+    traces.push(currentTrace);
+
+    var layout = {
+        title: 'PROBABILITY EXCEDENCE',
+        xaxis: { title: 'X-Axis' },
+        yaxis: { title: 'Y-Axis' },
+        paper_bgcolor: 'rgb(233, 233, 233)',
+        plot_bgcolor: 'rgb(233, 233, 233)',
+        legend: { font: { size: 16 } }
+    };
+
+    // Create the cumulative plot inside the 'chart' div
+    Plotly.newPlot('chart', traces, layout);
+}
+
+
+        function createCumulativePlot(xData, yData,colordata) {
+    var traces = [];
+    var currentTrace = {
+        x: [],
+        y: [],
+        type: 'scatter',
+        mode: 'lines+markers',
+        name: colordata[0],
+        marker: {
+            color: getRandomColor(), // Initial color for the cumulative plot
+        },
+    };
+
+    for (var i = 0; i < xData.length; i++) {
+        currentTrace.x.push(xData[i]);
+        currentTrace.y.push(yData[i]);
+
+        // Check if the next y-value is smaller, indicating a new line
+        if (colordata[i+1] != colordata[i]) {
+            traces.push(currentTrace);
+
+            currentTrace = {
+                x: [],
+                y: [],
+                type: 'scatter',
+                mode: 'lines+markers',
+                name: colordata[i+1],
+                marker: {
+                    color: getRandomColor(), // Assign a new color for the new line segment
+                },
+            };
+        }
+    }
+
+    // Push the last trace
+    traces.push(currentTrace);
+
+    var layout = {
+        title: 'CUMULATIVE DISTRIBUTION',
+        xaxis: { title: 'X-Axis' },
+        yaxis: { title: 'Y-Axis' },
+        paper_bgcolor: 'rgb(233, 233, 233)',
+        plot_bgcolor: 'rgb(233, 233, 233)',
+        legend: { font: { size: 16 } }
+    };
+
+    // Create the cumulative plot inside the 'chart' div
+    Plotly.newPlot('chart', traces, layout);
+}
+
+
+// Function to generate a random color
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+    </script>
 </body>
 </html>
-
-
-
-
